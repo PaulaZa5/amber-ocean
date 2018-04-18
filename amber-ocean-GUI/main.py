1219a3b8569b306367e3b090559b181f4b8ba7cf
Mina Mourice <minamourice2@gmail.com>
2018-04-18 04:03:07 +0200

    Add Registeration and Forgetten Password

--- amber-ocean-GUI/main.py
+++ amber-ocean-GUI/main.py
@@ -3,17 +3,45 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 from  kivy.graphics.vertex_instructions import Ellipse
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
 kivy.require('1.9.1')
 
-class AmberOceanLogin(Widget):
+			
+########### Login ###########
+class Login(Widget):
     pass
 
-class AmberOceanUI(Widget):
+class LoginScreen(Screen):
     pass
 
+########### Registeration ###########
+class Registeration(Widget):
+    pass
+
+class RegisterationScreen(Screen):
+    pass
+
+########### Forgetten Paswword ###########
+class ForgetPassword(Widget):
+    pass
+
+class ForgetPasswordScreen(Screen):
+    pass
+
+########### Screen Manager ###########
+class ScreenManagement(ScreenManager):
+    pass
+
+########### Load .kv File ###########
+AmberOcean = Builder.load_file("amberocean.kv")
+
+########### Build Class ###########
 class AmberOceanApp(App):
     def build(self):
-        return AmberOceanUI()
+        return AmberOcean
 
+########### Run Build ###########
 if __name__ == '__main__':
     AmberOceanApp().run()
