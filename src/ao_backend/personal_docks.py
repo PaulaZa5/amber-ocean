bed84aa03107c0eee59599d00e865cacf46af78f
Menna Mohamed Abdallah <32853257+MennaAbdallah@users.noreply.github.com>
2018-04-19 16:18:52 +0200

    Update personal_docks.py

--- src/ao_backend/personal_docks.py
+++ src/ao_backend/personal_docks.py
@@ -90,110 +90,189 @@ class PersonalDock(amber.AmberObject):
         self.sailed_ships = []  # Tuples of two elements (ship id, initial sailing date)
         self.join_date = dt.datetime.utcnow().date()
 
+    def check_password(self, password):
+        password = hashlib.sha224(password).hexdigest()
+        return self.password is password
+
     def deactivate_account(self):
-        pass
+        self.active = False
+        return True
 
-    def change_name(self, password, new_name):
-        pass
+    def change_name(self, new_name):
+        self.name = new_name
+        return True
 
-    def change_gender(self, password, new_gender):
-        pass
+    def change_gender(self, new_gender):
+        self.gender = new_gender
+        return True
 
-    def change_birthday(self, password, new_birthday):
-        pass
+    def change_birthday(self, new_birthday):
+        self.birthday = new_birthday
+        return True
 
-    def change_password(self, password, new_password):
-        pass
+    def change_password(self, new_password):
+        self.password = hashlib.sha224(new_password).hexdigest()
+        return True
 
-    def change_master_email(self, password, new_email):
-        pass
+    def change_master_email(self, new_email):
+        self.master_email = new_email
+        return True
 
     def add_email(self, new_email):
-        pass
+        self.emails.append(new_email)
+        return True
 
     def remove_email(self, email):
-        pass
+        self.emails.remove(email)
+        return True
 
-    def change_master_phone_number(self, password, new_phone_number):
-        pass
+    def change_master_phone_number(self, new_phone_number):
+        self.master_phone_number = new_phone_number
+        return True
 
     def add_phone_number(self, new_phone_number):
-        pass
+        self.phone_numbers.append(new_phone_number)
+        return True
 
     def remove_phone_number(self, phone_number):
-        pass
+        self.phone_numbers.remove(phone_number)
+        return True
 
     def add_friend(self, friend_id):
-        pass
+        self.friends.append(friend_id)
+        return True
 
     def remove_friend(self, friend_id):
-        pass
+        self.friends.remove(friend_id)
+        return True
 
     def add_follower(self, follower_id):
-        pass
+        self.followers.append(follower_id)
+        return True
 
     def remove_follower(self, follower_id):
-        pass
+        self.followers.remove(follower_id)
+        return True
 
     def add_followee(self, followee_id):
-        pass
+        self.followees.append(followee_id)
+        return True
 
     def remove_followee(self, followee_id):
-        pass
+        self.followees.remove(followee_id)
+        return True
 
     def add_family_member_relationship(self, family_member_id, family_member_relation):
-        pass
+        self.family.append((family_member_id, family_member_relation))
+        return True
 
     def edit_family_member_relationship(self, family_member_id, new_family_member_relation):
-        pass
+        for fam_member, fam_relation in self.family:
+            if fam_member is family_member_id:
+                fam_relation = new_family_member_relation
+                return True
+        return False
 
     def remove_family_member_relationship(self, family_member_id):
-        pass
+        for fam_member, fam_relation in self.family:
+            if fam_member is family_member_id:
+                self.family.remove((family_member_id, fam_relation))
+                return True
+        return False
 
     def add_education(self, major, place_of_education, starting_date, finishing_date):
-        pass
+        self.education.append((major, place_of_education, starting_date, finishing_date))
+        return True
 
     def edit_education(self, major, place_of_education, new_starting_date, new_finishing_date):
-        pass
+        for maj, place, start, finish in self.education:
+            if maj is major and place is place_of_education:
+                start = new_starting_date
+                finish = new_finishing_date
+                return True
+        return False
 
     def remove_education(self, major, place_of_education):
-        pass
+        for maj, place, start, finish in self.education:
+            if maj is major and place is place_of_education:
+                self.education.remove((maj, place, start, finish))
+                return True
+        return False
 
     def add_living_place(self, location, starting_date, finishing_date):
-        pass
+        self.living_in.append((location, starting_date, finishing_date))
+        return True
 
     def edit_living_place(self, location, new_starting_date, new_finishing_date):
-        pass
+        for loc, start, finish in self.living_in:
+            if loc is location:
+                start = new_starting_date
+                finish = new_finishing_date
+                return True
+        return False
 
     def remove_living_place(self, location):
-        pass
+        for loc, start, finish in self.living_in:
+            if loc is location:
+                self.living_in.remove((loc, start, finish))
+                return True
+        return False
 
     def add_relationship(self, significant_other_id, type, starting_date, finishing_date):
-        pass
+        self.relationships.append((significant_other_id, type, starting_date, finishing_date))
+        return True
 
     def edit_relationship(self, significant_other_id, new_type, new_starting_date, new_finishing_date):
-        pass
+        for sign_id, rel_type, start, finish in self.relationships:
+            if sign_id is significant_other_id:
+                rel_type = new_type
+                start = new_starting_date
+                finish = new_finishing_date
+                return True
+        return False
 
     def remove_relationship(self, significant_other_id):
-        pass
+        for sign_id, rel_type, start, finish in self.relationships:
+            if sign_id is significant_other_id:
+                self.relationships.remove((sign_id, rel_type, start, finish))
+                return True
+        return False
 
     def add_link(self, link_string, link_url):
-        pass
+        self.links.append((link_string, link_url))
+        return True
 
     def edit_link_url(self, link_string, new_link_url):
-        pass
+        for link_str, link_url in self.links:
+            if link_str is link_string:
+                link_url = new_link_url
+                return True
+        return False
 
     def remove_link(self, link_string):
-        pass
+        for link_str, link_url in self.links:
+            if link_str is link_string:
+                self.links.remove((link_str, link_url))
+                return True
+        return False
 
     def add_special_field(self, special_field_string, special_field_text):
-        pass
+        self.special_fields.append((special_field_string, special_field_text))
+        return True
 
     def edit_special_field_text(self, special_field_string, new_special_field_text):
-        pass
+        for field_str, field_txt in self.special_fields:
+            if field_str is special_field_string:
+                field_txt = new_special_field_text
+                return True
+        return False
 
     def remove_special_field(self, special_field_string):
-        pass
+        for field_str, field_txt in self.special_fields:
+            if field_str is special_field_string:
+                self.special_fields.remove((field_str, field_txt))
+                return True
+        return False
 
     def generate_ships(self):
 
@@ -204,10 +283,15 @@ class PersonalDock(amber.AmberObject):
         pass
 
     def sail_ship_to_this_dock(self, ship_id):
-        pass
+        self.sailed_ships.append((ship_id, dt.datetime.utcnow()))
+        return True
 
     def sink_ship_from_this_dock(self, ship_id):
-        pass
+        for ship, date_time in self.sailed_ships:
+            if ship is ship_id:
+                self.sailed_ships.remove((ship, date_time))
+                return True
+        return False
 
     @staticmethod
     def import_from_database(line):
