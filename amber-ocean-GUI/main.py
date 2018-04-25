00442e9e8cfe5e1d77c533ec5da95ee85bfa2cef
Mina Mourice <minamourice2@gmail.com>
2018-04-25 02:27:20 +0200

    Add Home Page GUI

--- amber-ocean-GUI/main.py
+++ amber-ocean-GUI/main.py
@@ -1,4 +1,19 @@
+import os
+os.environ['KIVY_GL_BACKEND'] = 'sdl2'
+
+from kivy.core.window import Window
 import kivy
+kivy.require('1.0.6')
+from kivy.core.window import Window
+from glob import glob
+from random import randint
+from os.path import join, dirname
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.scatter import Scatter
+from kivy.properties import StringProperty
+# FIXME this shouldn't be necessary
+from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
@@ -6,7 +21,7 @@ from  kivy.graphics.vertex_instructions import Ellipse
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-kivy.require('1.9.1')
+
 
 			
 ########### Login ###########
