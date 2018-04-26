5eeb6dc9685536214db9fddbcab00694bc7e8f6e
BeshoyAnwar <32778960+BeshoyAnwar@users.noreply.github.com>
2018-04-26 20:34:18 +0200

    import,export(datetime)
    update datetime attributes with .now() 
    update export,import to resolve datetime problems

--- src/ao_backend/ships.py
+++ src/ao_backend/ships.py
@@ -4,7 +4,7 @@ This file contains all ships related operations
 
 import amber
 import datetime as dt
-
+import math
 
 class ContentType(enumerate):
     Text = 'text'
@@ -35,15 +35,16 @@ class Ship(amber.AmberObject):
     """
 
     @staticmethod
-    def RegisterShip(creator_id, where_is_it_created_id, content_type, txt_content, image_content=None, video_content=None,
-                     privacy=ShipPrivacy.Everyone, parent_ship_id=None):
-        new = Ship(creator_id, where_is_it_created_id, content_type, txt_content, image_content, video_content, privacy, parent_ship_id)
+    def RegisterShip(creator_id, where_is_it_created_id, content_type, txt_content, image_content=None,
+                     video_content=None, privacy=ShipPrivacy.Everyone, parent_ship_id=None, new_object=True):
+        new = Ship(creator_id, where_is_it_created_id, content_type, txt_content, image_content, video_content,
+                   privacy, parent_ship_id, new_object)
         amber.database[new.id] = new
         return new
 
     def __init__(self, creator_id, where_is_it_created_id, content_type, txt_content, image_content=None, video_content=None,
-                 privacy=ShipPrivacy.Everyone, parent_ship_id=None):
-        super().__init__()
+                 privacy=ShipPrivacy.Everyone, parent_ship_id=None, new_object=True):
+        super().__init__(new_object)
         self.creator_id = creator_id
         self.where_is_it_created_id = where_is_it_created_id
         self.content_type = content_type
@@ -54,7 +55,8 @@ class Ship(amber.AmberObject):
         self.reactions = {Reactions.Like: [], Reactions.Dislike: [], Reactions.Love: [], Reactions.Angry: [],
                           Reactions.Haha: []}  # Add ids of personal docks in the list
         self.parent_ship_id = parent_ship_id
-        self.creation_date = dt.datetime.utcnow().timetuple()
+        self.creation_date = dt.datetime.utcnow().now()
+        self.creation_date = self.creation_date.replace(microsecond=0)
         self.child_ships = []
         self.edit_history = []  # Tuples of four elements (text content, image content, video content, creation date)
 
@@ -80,31 +82,139 @@ class Ship(amber.AmberObject):
                     return True
         return False
 
-    def change_reaction(self, reactioner_id, new_reaction):
-        pass
-
-    def remove_reaction(self, reactioner_id):
-        pass
-
     def commit_edit(self, edit_text, edit_image=None, edit_video=None):
         self.edit_history.append((self.txt_content, self.image_content, self.video_content, self.creation_date))
         self.txt_content = edit_text
         self.image_content = edit_image
         self.video_content = edit_video
         self.creation_date = dt.datetime.utcnow().timetuple()
+        return True
 
     def change_privacy(self, new_privacy):
         self.privacy = new_privacy
         return True
 
+    def comments_no(self):
+        return len(self.child_ships)
+
+    def __len__(self):
+        return len(self.child_ships)
+
+    def generate_comments(self):
+
+        """
+        Starts yielding comments on this ship
+        """
+
+        for ship in self.child_ships:
+            yield ship
+
     @staticmethod
-    def import_from_database(line):
-        loaded_ship = Ship()
-        return loaded_ship
+    def import_from_database(inData):
+        f = "%Y-%m-%d %H:%M:%S"
+        lines = inData.split('>\n')
+        file = open('dataout.xml', 'w')
+        for line in lines:
+            line = line.replace('>', '')
+            if not (line.find("</") == -1):
+                line = line.replace("</" + temp, '')
+            else:
+                line = line.replace('<', '')
+                temp = line
+            file.write(line)
+        file.close()
+        file = open('dataout.xml', 'r')
+        lines = iter(file.readlines())
+        loadedShip = Ship("", "", "", "", new_object=False)
+        for line in lines:
+            attribute = line[0:line.find("\t")]
+            attributeValue = line[line.find("\t") + 1:line.find("\n")]
+            if attributeValue == "True":
+                loadedShip.attribute = True
+            elif attributeValue == "False":
+                loadedShip.attribute = False
+            elif  (attributeValue.find(":") != -1) and attribute=='reactions':
+                line = line.replace("reactions\t", "")
+                dictlines = line.split(";")
+                for dictline in dictlines:
+                    dictlist = dictline.split(":")
+                    valueslist = dictlist[1].split(",")
+                    if not (valueslist[0] == '\n'):
+                        loadedShip.reactions[dictlist[0]] = valueslist
+            elif not (attributeValue.find("(") == -1):
+                tuplesvalue = [tuple(i for i in element.strip('()').split(',')) for element in
+                               attributeValue.split('),(')]
+                for i in range(len(tuplesvalue)):
+                    for j in range(len(tuplesvalue[i])):
+                        if "datetime.datetime" in tuplesvalue[i][j]:
+                            tupvalue = tuplesvalue[i][j].replace('datetime.datetime', '')
+                            date = dt.datetime.strptime(tupvalue, f)
+                            tuplesvalue[i] = list(tuplesvalue[i])
+                            tuplesvalue[i][j] = date
+                            tuplesvalue[i] = tuple(tuplesvalue[i])
+                setattr(loadedShip, attribute, tuplesvalue)
+            elif not (attributeValue.find(",") == -1):
+                listvalue = attributeValue.split(',')
+                setattr(loadedShip, attribute, listvalue)
+
+
+            else:
+                if "datetime.datetime" in attributeValue:
+                    attributeValue=attributeValue.replace('datetime.datetime','')
+                    date = dt.datetime.strptime(attributeValue, f)
+                    setattr(loadedShip, attribute, date)
+                else:
+                    setattr(loadedShip, attribute, attributeValue)
+
+        return loadedShip
 
     def export_to_database(self):
         line = str()
-        return line
+        for attribute, attributeValue in vars(self).items():
+            if attribute == "id" or attributeValue == None:
+                continue
+            if not (isinstance(attributeValue, bool)) and not (isinstance(attributeValue, dt.date)):
+                if len(attributeValue) == 0:
+                    continue
+            line += "<" + attribute + ">"
 
+            if type(attributeValue) is dict:
+                line += "\n" + "\t"
+                for key, dictlist in attributeValue.items():
+
+                    line += key + ":"
+                    for value in dictlist:
+                        line += str(value) + ","
+                    if line[len(line) - 1] == ",":
+                        line = line[:-1]  # to remove the last "," in the line
+                    line += ";"
+                if line[len(line) - 1] == ";":
+                    line = line[:-1]  # to remove the last ";" in the line
+            elif type(attributeValue) is list:
+                line += "\n" + "\t"
+                for value in attributeValue:
+                    if type(value) is tuple:
+                        line += "("
+
+                        for x in value:
+                            attrstring = str(x)
+                            if isinstance(x, dt.datetime):
+                                attrstring = "datetime.datetime" + attrstring
+                            line += attrstring +","
+
+                        line = line[:-1]  # to remove the last "," in the line
+                        line += "),"
+                    else:
+                        line += str(value) + ","
+                if line[len(line) - 1] == ",":
+                    line = line[:-1]  # to remove the last "," in the line
+            else:
+                attrstring = str(attributeValue)
+                if isinstance(attributeValue, dt.datetime):
+                    attrstring = "datetime.datetime" + attrstring
+                line += "\n" + "\t" + attrstring
+
+            line += "\n" + "</" + attribute + ">" + "\n"
+        return line
     def export_to_xml(self):
         pass
