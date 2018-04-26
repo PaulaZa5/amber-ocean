9c279a3968f60bb5474cdc6d08db0795cf17b851
BeshoyAnwar <32778960+BeshoyAnwar@users.noreply.github.com>
2018-04-26 20:33:21 +0200

    import,export(datetime)/edit tuples methods
    update datetime attributes with .now() 
    update export,import to resolve datetime problems
    solve bugs in edit tubles methods

--- src/ao_backend/personal_docks.py
+++ src/ao_backend/personal_docks.py
@@ -1,12 +1,11 @@
 """
 This file contains all personal-docks related operations
 """
-
 import amber
+from amber import database
 import datetime as dt
 import hashlib
-
-
+#import sortedcontainers
 class FamilyRelationship(enumerate):
     Brother = 'brother'
     Sister = 'sister'
@@ -45,7 +44,7 @@ class RelationshipStates(enumerate):
     Single = 'single'
     In_a_relationship = 'inarelationship'
     Engaged = 'engaged'
-    Married = 'marriend'
+    Married = 'married'
     Separated = 'separated'
     In_an_open_relationship = 'inanopenrelationship'
     Complicated = 'complicated'
@@ -60,17 +59,17 @@ class PersonalDock(amber.AmberObject):
     """
 
     @staticmethod
-    def RegisterAccount(name, gender, birthday, password, email=None, phone_number=None):
-        new = PersonalDock(name, gender, birthday, password, email, phone_number)
+    def RegisterAccount(name, gender, birthday, password, email=None, phone_number=None, new_object=True):
+        new = PersonalDock(name, gender, birthday, password, email, phone_number, new_object)
         amber.database[new.id] = new
         return new
 
-    def __init__(self, name, gender, birthday, password, email=None, phone_number=None):
-        super().__init__()
+    def __init__(self, name, gender, birthday, password, email=None, phone_number=None, new_object=True):
+        super().__init__(new_object)
         self.name = name
         self.gender = gender
         self.birthday = birthday  # dt.datetime.date()
-        self.password = hashlib.sha224(password).hexdigest()  # Encrypted password
+        self.password = hashlib.sha224(password.encode('utf-8')).hexdigest()  # Encrypted password
         self.master_email = email
         self.emails = [email]
         self.master_phone_number = phone_number
@@ -88,126 +87,383 @@ class PersonalDock(amber.AmberObject):
         self.links = []  # Tuples of two elements (link string=>(facebook, linkedin..etc), link url)
         self.special_fields = []  # Tuples of two elements (field string=>(nickname, about..etc), field text)
         self.sailed_ships = []  # Tuples of two elements (ship id, initial sailing date)
-        self.join_date = dt.datetime.utcnow().date()
+        self.join_date = dt.datetime.utcnow().now()
+        self.join_date = self.join_date.replace(microsecond=0)
+
+
+    def check_password(self, password):
+        password = hashlib.sha224(password.encode('utf-8')).hexdigest()
+        return self.password is password
 
     def deactivate_account(self):
-        pass
+        self.active = False
+        return True
 
-    def change_name(self, password, new_name):
-        pass
+    def change_name(self, new_name):
+        self.name = new_name
+        return True
 
-    def change_gender(self, password, new_gender):
-        pass
+    def change_gender(self, new_gender):
+        self.gender = new_gender
+        return True
 
-    def change_birthday(self, password, new_birthday):
-        pass
+    def change_birthday(self, new_birthday):
+        self.birthday = new_birthday
+        return True
 
-    def change_password(self, password, new_password):
-        pass
+    def change_password(self, new_password):
+        self.password = hashlib.sha224(new_password.encode('utf-8')).hexdigest()
+        return True
 
-    def change_master_email(self, password, new_email):
-        pass
+    def change_master_email(self, new_email):
+        self.master_email = new_email
+        return True
 
     def add_email(self, new_email):
-        pass
+        self.emails.append(new_email)
+        return True
 
     def remove_email(self, email):
-        pass
+        self.emails.remove(email)
+        return True
 
-    def change_master_phone_number(self, password, new_phone_number):
-        pass
+    def change_master_phone_number(self, new_phone_number):
+        self.master_phone_number = new_phone_number
+        return True
 
     def add_phone_number(self, new_phone_number):
-        pass
+        self.phone_numbers.append(new_phone_number)
+        return True
 
     def remove_phone_number(self, phone_number):
-        pass
+        self.phone_numbers.remove(phone_number)
+        return True
 
     def add_friend(self, friend_id):
-        pass
+        self.friends.append(friend_id)
+        return True
 
     def remove_friend(self, friend_id):
-        pass
+        self.friends.remove(friend_id)
+        return True
 
     def add_follower(self, follower_id):
-        pass
+        self.followers.append(follower_id)
+        return True
 
     def remove_follower(self, follower_id):
-        pass
+        self.followers.remove(follower_id)
+        return True
 
     def add_followee(self, followee_id):
-        pass
+        self.followees.append(followee_id)
+        return True
 
     def remove_followee(self, followee_id):
-        pass
+        self.followees.remove(followee_id)
+        return True
 
     def add_family_member_relationship(self, family_member_id, family_member_relation):
-        pass
+        self.family.append((family_member_id, family_member_relation))
+        return True
 
     def edit_family_member_relationship(self, family_member_id, new_family_member_relation):
-        pass
+        exist=self.remove_family_member_relationship( family_member_id)
+        if exist:
+            self.add_family_member_relationship( family_member_id, new_family_member_relation)
+        return exist
 
     def remove_family_member_relationship(self, family_member_id):
-        pass
+        for fam_member, fam_relation in self.family:
+            if fam_member is family_member_id:
+                self.family.remove((family_member_id, fam_relation))
+                return True
+        return False
 
     def add_education(self, major, place_of_education, starting_date, finishing_date):
-        pass
+        self.education.append((major, place_of_education, starting_date, finishing_date))
+        return True
 
     def edit_education(self, major, place_of_education, new_starting_date, new_finishing_date):
-        pass
+        exist = self.remove_education( major, place_of_education)
+        if exist:
+            self.add_education( major, place_of_education, new_starting_date, new_finishing_date)
+        return exist
 
     def remove_education(self, major, place_of_education):
-        pass
+        for maj, place, start, finish in self.education:
+            if maj is major and place is place_of_education:
+                self.education.remove((maj, place, start, finish))
+                return True
+        return False
 
     def add_living_place(self, location, starting_date, finishing_date):
-        pass
+        self.living_in.append((location, starting_date, finishing_date))
+        return True
 
     def edit_living_place(self, location, new_starting_date, new_finishing_date):
-        pass
+        exist = self.remove_living_place( location)
+        if exist:
+            self.add_living_place( location, new_starting_date, new_finishing_date)
+        return exist
 
     def remove_living_place(self, location):
-        pass
+        for loc, start, finish in self.living_in:
+            if loc is location:
+                self.living_in.remove((loc, start, finish))
+                return True
+        return False
 
     def add_relationship(self, significant_other_id, type, starting_date, finishing_date):
-        pass
+        self.relationships.append((significant_other_id, type, starting_date, finishing_date))
+        return True
 
     def edit_relationship(self, significant_other_id, new_type, new_starting_date, new_finishing_date):
-        pass
+        exist = self.remove_relationship( significant_other_id)
+        if exist:
+            self.add_relationship( significant_other_id, new_type, new_starting_date, new_finishing_date)
+        return exist
 
     def remove_relationship(self, significant_other_id):
-        pass
+        for sign_id, rel_type, start, finish in self.relationships:
+            if sign_id is significant_other_id:
+                self.relationships.remove((sign_id, rel_type, start, finish))
+                return True
+        return False
 
     def add_link(self, link_string, link_url):
-        pass
+        self.links.append((link_string, link_url))
+        return True
 
     def edit_link_url(self, link_string, new_link_url):
-        pass
+        exist = self.remove_link( new_link_url)
+        if exist:
+            self.add_link( link_string, new_link_url)
+        return exist
 
     def remove_link(self, link_string):
-        pass
+        for link_str, link_url in self.links:
+            if link_str is link_string:
+                self.links.remove((link_str, link_url))
+                return True
+        return False
 
     def add_special_field(self, special_field_string, special_field_text):
-        pass
+        self.special_fields.append((special_field_string, special_field_text))
+        return True
 
     def edit_special_field_text(self, special_field_string, new_special_field_text):
-        pass
+        exist = self.remove_special_field( special_field_string)
+        if exist:
+            self.add_special_field( special_field_string, new_special_field_text)
+        return exist
 
     def remove_special_field(self, special_field_string):
-        pass
+        for field_str, field_txt in self.special_fields:
+            if field_str is special_field_string:
+                self.special_fields.remove((field_str, field_txt))
+                return True
+        return False
+
+    def generate_ships(self):
+
+        """
+        Starts yielding posts shared to this dock chronologically
+        """
+        for shipid,shipdate in self.sailed_ships:
+            yield shipid
 
     def sail_ship_to_this_dock(self, ship_id):
-        pass
+        self.sailed_ships.append((ship_id, dt.datetime.utcnow()))
+        return True
 
     def sink_ship_from_this_dock(self, ship_id):
-        pass
+        for ship, date_time in self.sailed_ships:
+            if ship is ship_id:
+                self.sailed_ships.remove((ship, date_time))
+                return True
+        return False
+
+    def join_sea(self,sea_id):
+        self.seas.append(sea_id)
+
+    def leave_sea(self,sea_id):
+        self.seas.remove(sea_id)
+
+    def max_reactions_ship(self):
+        maxreactions=0
+        maxreactions_id=-1
+        for ship in self.generate_ships():
+            reactions=0
+            for key, dictlist in database[ship.id].reactions.items():
+                reactions+=len(dictlist)
+            if reactions>maxreactions:
+                maxreactions=reactions
+                maxreactions_id=ship.id
+        return maxreactions_id,maxreactions
+
+    def max_comments_ship(self):
+        maxcomments=0
+        maxcomments_id=-1
+        for ship in self.generate_ships():
+            if len(database[ship.id].child_ships)>maxcomments:
+                maxcomments=len(database[ship.id].child_ships)
+                maxcomments_id=ship.id
+        return maxcomments_id,maxcomments
+
+    def docks_you_may_know(self):
+
+        """
+        Function that generates tuples of (id,matual_friends_no ) of other docks the input-dock doesn't know after
+        building a database of docks of friends of friends using graph breadth first traversal
+        """
+        recommendations = {}
+        for friend in self.friends:
+            for vertex in database[friend].friends:
+                if vertex in recommendations.keys():
+                    recommendations[vertex] += 1
+                else:
+                    recommendations[vertex] = 1
+
+        sorted_recommendations = sorted(recommendations.items(), key=lambda tup: tup[1], reverse=True)
+
+        for recommendation in sorted_recommendations:
+            yield recommendation
+
+    def seas_you_might_join(self):
+
+        """
+        Function that generates tuples of (id,friends_members_no ) of seas the input-dock hasn't joined after building
+        a database of friends's seas using graph breadth first traversal
+        """
+        recommendations = {}
+        for friend in self.friends:
+            for group in database[friend].groups:
+                if group in recommendations.keys():
+                    recommendations[group] += 1
+                else:
+                    recommendations[group] = 1
+
+        sorted_recommendations = sorted(recommendations.items(), key=lambda tup: tup[1], reverse=True)
+
+        for recommendation in sorted_recommendations:
+            yield recommendation
+
+    def newsfeed_ships(self):
+
+        """
+        Function that generates ids of ships that was posted to friends' docks and joined
+        seas chronologically
+        """
+        posts = []
+        # post is a list of tuples (post_id,post_no_in_sailedships_list)
+        for friend in self.friends:
+            friend_posts=database[friend].sailed_ships
+            no_of_posts=len(friend_posts)
+            newest_post_id=friend_posts[no_of_posts-1][0]
+            posts.add((newest_post_id,no_of_posts-1))
+        for group in self.groups:
+            group_posts = database[group].sailed_ships
+            no_of_posts = len(group_posts)
+            newest_post_id = group_posts[no_of_posts - 1][0]
+            posts.add((newest_post_id, no_of_posts - 1))
+
+        sorted_posts = sortedcontainers.SortedListWithKey(posts, key=lambda tup: database[tup[0]].creation_date)
+
+        for post_id, post_no in sorted_posts:
+            yield post_id
+            sorted_posts.pop()
+            if post_no != 0:
+                post_creator_id=database[post_id].creator_id
+                posts = database[post_creator_id].sailed_ships
+                new_post_id=posts[post_no-1][0]
+                sorted_posts((new_post_id,post_no-1))
+
 
     @staticmethod
-    def import_from_database(line):
-        loaded_personal_dock = PersonalDock()
-        return loaded_personal_dock
+    def import_from_database(inData):
+        f = "%Y-%m-%d %H:%M:%S"
+        lines = inData.split('>\n')
+        file = open('dataout.xml', 'w')
+        for line in lines:
+            line = line.replace('>', '')
+            if not (line.find("</") == -1):
+                line = line.replace("</" + temp, '')
+            else:
+                line = line.replace('<', '')
+                temp = line
+            file.write(line)
+        file.close()
+        file = open('dataout.xml', 'r')
+        lines = iter(file.readlines())
+        loadedDock = PersonalDock("", "", "", "", new_object=False)
+        for line in lines:
+            attribute = line[0:line.find("\t")]
+            attributeValue = line[line.find("\t") + 1:line.find("\n")]
+            if attributeValue == "True":
+                loadedDock.attribute = True
+            elif attributeValue == "False":
+                loadedDock.attribute = False
+            elif not (attributeValue.find("(") == -1):
+                tuplesvalue = [tuple(i for i in element.strip('()').split(',')) for element in attributeValue.split('),(')]
+                for i in range(len(tuplesvalue)):
+                    for j in range(len(tuplesvalue[i])):
+                        if  "datetime.datetime" in tuplesvalue[i][j]:
+                            tupvalue=tuplesvalue[i][j].replace('datetime.datetime','')
+                            date=dt.datetime.strptime(tupvalue, f)
+                            tuplesvalue[i]=list(tuplesvalue[i])
+                            tuplesvalue[i][j]=date
+                            tuplesvalue[i]=tuple(tuplesvalue[i])
+                setattr(loadedDock, attribute, tuplesvalue)
+            elif not (attributeValue.find(",") == -1):
+                listvalue = attributeValue.split(',')
+                setattr(loadedDock, attribute, listvalue)
+
+            else:
+                if "datetime.datetime" in attributeValue:
+                    attributeValue=attributeValue.replace('datetime.datetime','')
+                    date = dt.datetime.strptime(attributeValue, f)
+                    setattr(loadedDock, attribute, date)
+                else:
+                    setattr(loadedDock, attribute, attributeValue)
+
+        return loadedDock
+
 
     def export_to_database(self):
         line = str()
+        for attribute, attributeValue in vars(self).items():
+            if attribute == "id" or attributeValue == None:
+                continue
+            if not (isinstance(attributeValue, bool)) and not (isinstance(attributeValue, dt.date)):
+                if len(attributeValue) == 0:
+                    continue
+            line += "<" + attribute + ">"
+            line += "\n" + "\t"
+            if type(attributeValue) is list:
+                for value in attributeValue:
+                    if type(value) is tuple:
+                        line += "("
+
+                        for x in value:
+                            attrstring = str(x)
+                            if isinstance(x, dt.datetime):
+                                attrstring = "datetime.datetime" + attrstring
+                            line += attrstring +","
+
+                        line = line[:-1]  # to remove the last "," in the line
+                        line += "),"
+                    else:
+                        line += str(value) + ","
+                if line[len(line) - 1] == ",":
+                    line = line[:-1]  # to remove the last "," in the line
+            else:
+                attrstring=str(attributeValue)
+                if isinstance(attributeValue, dt.datetime):
+                    attrstring="datetime.datetime"+attrstring
+                line += attrstring
+
+            line += "\n" + "</" + attribute + ">" + "\n"
         return line
 
     def export_to_xml(self):
